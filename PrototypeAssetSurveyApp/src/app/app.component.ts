ccd8dbeef12c5b24d65781b3e4e5027c8e0ef426
rakoc001 <rakoc001@users.noreply.github.com>
2019-08-02 18:12:28 +0100

    Firebase plugin added successfully without creating additional errors

--- PrototypeAssetSurveyApp/src/app/app.component.ts
+++ PrototypeAssetSurveyApp/src/app/app.component.ts
@@ -5,4 +5,18 @@ import { Component } from "@angular/core";
     selector: "ns-app",
     templateUrl: "app.component.html"
 })
-export class AppComponent { }
+export class AppComponent implements OnInit{
+    ngOnInit() {
+        firebase.init({
+            // Optionally pass in properties for database, authentication and cloud messaging,
+            // see their respective docs
+        }).then(
+            () => {
+                console.log("firebase.init done");
+            },
+            error => {
+              console.log(`firebase.init error: ${error}`);
+            }
+        );
+    }
+}
