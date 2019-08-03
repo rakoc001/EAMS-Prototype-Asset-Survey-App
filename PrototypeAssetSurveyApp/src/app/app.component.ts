69d869c0823589b1f33137005352a0d66279f353
rakoc001 <rakoc001@users.noreply.github.com>
2019-08-03 14:07:07 +0100

    Resolved errors in firebase inclusion

--- PrototypeAssetSurveyApp/src/app/app.component.ts
+++ PrototypeAssetSurveyApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+const firebase = require("nativescript-plugin-firebase");
 
 @Component({
     moduleId: module.id,
@@ -8,8 +10,8 @@ import { Component } from "@angular/core";
 export class AppComponent implements OnInit{
     ngOnInit() {
         firebase.init({
-            // Optionally pass in properties for database, authentication and cloud messaging,
-            // see their respective docs
+            // Optionally pass in properties for database, authentication and
+            // cloud messaging, see their respective docs
         }).then(
             () => {
                 console.log("firebase.init done");
