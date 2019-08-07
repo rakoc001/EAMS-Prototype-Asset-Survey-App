61a6bfe94e25d63c7f079d4795919dcb0e6f6ef2
rakoc001 <rakoc001@users.noreply.github.com>
2019-08-07 17:21:15 +0100

    Updating "Home" page content

--- PrototypeAssetSurveyAppDrawer/src/app/home/home.component.ts
+++ PrototypeAssetSurveyAppDrawer/src/app/home/home.component.ts
@@ -17,6 +17,22 @@ export class HomeComponent implements OnInit {
         // Init your component properties here.
     }
 
+    createEntry() {
+        console.log("New Asset button pressed");
+    }
+
+    updateEntry() {
+        console.log("Update Asset button pressed");
+    }
+
+    reports() {
+        console.log("Create Reports button pressed");
+    }
+
+    search() {
+        console.log("Search button pressed");
+    }
+
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
