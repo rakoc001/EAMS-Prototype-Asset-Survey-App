c6c5c95c81d249f1ff839ed4cf268494410f17aa
rakoc001 <rakoc001@users.noreply.github.com>
2019-08-12 12:23:45 +0100

    Create login.routing.ts

---
+++ PrototypeAssetSurveyAppDrawer/src/login/login.routing.ts
@@ -0,0 +1,9 @@
+import { ModuleWithPRoviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { LoginComponent } from "./login.component";
+
+const loginRoutes: Routes = [
+    { path: "login", component: LoginComponent },
+];
+export const loginRouting: ModuleWithProviders = RouterModule.forChild(loginRoutes);
