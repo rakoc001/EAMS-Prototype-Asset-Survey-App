b96c45a7338e1dd7cf45670cc9e95be30d578508
rakoc001 <rakoc001@users.noreply.github.com>
2019-08-12 16:32:50 +0100

    Update create.component.ts
    Adding basic SQLite functionality to create asset page

--- PrototypeAssetSurveyAppDrawer/src/app/create/create.component.ts
+++ PrototypeAssetSurveyAppDrawer/src/app/create/create.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { NavigationEnd, Router } from "@angular/router";
 import { RouterExtensions } from "nativescript-angular/router";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import { filter } from "rxjs/operators";
 import * as app from "tns-core-modules/application";
+
+import { TextField } from "tns-core-modules/ui/text-field";
+import { ListViewEventData, RadListView } from "nativescript-ui-listview";
+import { View } from "tns-core-modules/ui/core/view"
+
+import { getString } from "tns-core-modules/application-settings";
+import { alert, LoginService, User } from "../../shared";
+import { DatabaseService } from "../../database/sqlite.service";
 
 /* ***********************************************************
 * Before you can navigate to this page from your app, you need to reference this page's module in the
@@ -27,16 +35,67 @@ export class CreateNewComponent implements OnInit {
     conditionListPickerIndex: number = 0;
     statusListPickerIndex: number = 0;
 
-    constructor(private router: Router, private routerExtensions: RouterExtensions) {
+    db: any;
+    assetList: Array<Object> = [];
+    asset = "";
+    user_id: string;
+    @ViewChild("assetTextField") assetTextField: ElementRef;
+
+    constructor(
+        private router: Router,
+        private routerExtensions: RouterExtensions,
+        private userService: LoginService,
+        private database: DatabaseService
+      ) {
+        this.user_id = getString("user_id");
         /* ***********************************************************
         * Use the constructor to inject app services that you need in this component.
         *************************************************************/
     }
 
     ngOnInit(): void {
+        this.selectItems();
         /* ***********************************************************
         * Use the "ngOnInit" handler to initialize data for this component.
         *************************************************************/
+    }
+
+    selectItems() {
+        this.assetList = [];
+        this.database.getdbConnection()
+            .then(db => {
+                db.all("SELECT id, item_name FROM items WHERE user_id = ?", [this.user_id]).then(rows => {
+                    for (var row in rows) {
+                        this.assetList.push({ id: rows[row][0], name: rows[row][1] });
+                    }
+                    this.db = db;
+                }, error => {
+                    console.log("SELECT ERROR", error);
+                });
+            });
+    }
+
+    logout() {
+        this.userService.logout();
+        this.routerExtensions.navigate(["/login"]);
+    }
+
+    add() {
+        if (this.asset.trim() ==="") {
+            alert("Ener a grocery item");
+            return;
+        }
+
+        let textField = <TextField>this.assetTextField.nativeElement;
+        textField.dismissSoftInput();
+
+        this.db.execSQL("INSERT INTO items (item_name, user_id) VALUES (?,?)", [this.asset, this.user_id]).thin(id => {
+            this.assetList.unshift({ id: id, name: this.asset });
+            this.asset = "";
+        }, error => {
+            alert('an error occurred while adding an item to your list.');
+            this.asset = "";
+        });
     }
 
     onNavItemTap(navItemRoute: string): void {
