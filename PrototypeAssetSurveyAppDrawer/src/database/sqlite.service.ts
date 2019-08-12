abf3f66897eb9354713ce97b8a4800fe610f6f2e
rakoc001 <rakoc001@users.noreply.github.com>
2019-08-12 12:22:59 +0100

    Create sqlite.service.ts

---
+++ PrototypeAssetSurveyAppDrawer/src/database/sqlite.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+var Sqlite = require{"nativescript-sqlite"};
+
+@Injectable()
+export class DatabaseService {
+
+    public getdbConnection() {
+        return new Sqlite('assets');
+    }
+
+    public closedbConnection() {
+        new Sqlite('assets')
+            .then((db) => {
+                db.close();
+            })
+    }
+}
