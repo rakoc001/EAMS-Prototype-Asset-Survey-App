bed837673a0f74450c24247ea3bc56efc213b13b
rakoc001 <rakoc001@users.noreply.github.com>
2019-08-12 16:28:06 +0100

    Update auth-guard.service.ts
    Correcting file path for BackendService

--- PrototypeAssetSurveyAppDrawer/src/app/auth-guard.service.ts
+++ PrototypeAssetSurveyAppDrawer/src/app/auth-guard.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Router, CanActivate } from "@angular/router";
 
-import { BackendService } from "/shared/backend.service";
+import { BackendService } from "../shared/backend.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
