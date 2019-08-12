ef8ebf410c9aa5f87747a5b4c840c3c3d45c3680
rakoc001 <rakoc001@users.noreply.github.com>
2019-08-12 16:22:20 +0100

    Update index.ts
    Corrected misspellings in index list

--- PrototypeAssetSurveyAppDrawer/src/shared/index.ts
+++ PrototypeAssetSurveyAppDrawer/src/shared/index.ts
@@ -2,4 +2,4 @@ export * from "./backend.service";
 export * from "./user.model";
 export * from "./login.service";
 export * from "./dialog-util";
-export * from "./status-bar-util";
+export * from "./status-bar-utils";
