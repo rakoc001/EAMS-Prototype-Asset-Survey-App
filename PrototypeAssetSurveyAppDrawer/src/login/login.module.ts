078b396e1c86783dae7a0ec480bec8482d6b95f9
rakoc001 <rakoc001@users.noreply.github.com>
2019-08-16 13:57:33 +0100

    Update login.module.ts

--- PrototypeAssetSurveyAppDrawer/src/login/login.module.ts
+++ PrototypeAssetSurveyAppDrawer/src/login/login.module.ts
@@ -5,7 +5,7 @@ import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 
 //import { LoginRoutingModule } from "./login-routing.module";
-import { loginRouting } from "./login/routing"
+import { loginRouting } from "./login.routing"
 import { LoginComponent } from "./login.component";
 
 @NgModule({
