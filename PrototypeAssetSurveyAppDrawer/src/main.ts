9a2c6ce6842da1b190de3561e74953f4e9b58d6a
rakoc001 <rakoc001@users.noreply.github.com>
2019-08-29 20:45:51 +0100

    Update main.ts
    Adding firebase.init code to main.ts to run at app startup

--- PrototypeAssetSurveyAppDrawer/src/main.ts
+++ PrototypeAssetSurveyAppDrawer/src/main.ts
@@ -4,5 +4,37 @@ import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { AppModule } from "./app/app.module";
 import { enableProdMode } from "@angular/core";
 
+import * as firebase from "nativescript-plugin-firebase";
+
+firebase.init({
+
+}).then(
+    () => {
+        console.log("firebase.init done");
+    },
+    error => {
+        console.log(`firebase.init error: ${error}`);
+    }
+);
+
+const listener = {
+    onAuthStateChanged(data) {
+        console.log(data.loggedIn ? "Logged in to firebase" : "Logged out from firebase");
+        if (data.loggedIn) {
+            console.log("User Info", data.user);
+        }
+    },
+    thisArg: this
+};
+
+// add the listener:
+firebase.addAuthStateListener(listener);
+
+// stop listening to auth state changes:
+firebase.removeAuthStateListener(listener);
+
+// check if already listening to auth state changes:
+firebase.hasAuthStateListener(listener);
+
 enableProdMode();
 platformNativeScriptDynamic().bootstrapModule(AppModule);
